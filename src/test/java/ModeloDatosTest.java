f866bbf8d04b94e18496d9e7f6c119a0fe163c73
yordanglez <yorda891216@gmail.com>
2024-06-17 23:38:38 +0200

    Prueba unitaria corregida

--- src/test/java/ModeloDatosTest.java
+++ src/test/java/ModeloDatosTest.java
@@ -1,14 +1,14 @@
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.*;
-//public class ModeloDatosTest {
-//@Test
-//public void testExisteJugador() {
-//System.out.println("Prueba de existeJugador");
-//String nombre = "";
-//ModeloDatos instance = new ModeloDatos();
-//boolean expResult = false;
-//boolean result = instance.existeJugador(nombre);
-////assertEquals(expResult, result);
-//fail("Fallo forzado.");
-//}
-//}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ModeloDatosTest {
+@Test
+public void testExisteJugador() {
+System.out.println("Prueba de existeJugador");
+String nombre = "";
+ModeloDatos instance = new ModeloDatos();
+boolean expResult = false;
+boolean result = instance.existeJugador(nombre);
+//assertEquals(expResult, result);
+fail("Fallo forzado.");
+}
+}
